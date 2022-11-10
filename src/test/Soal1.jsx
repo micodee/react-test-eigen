fe4f5fe912da5cb3a73facef50c0b275345b1c96
micodee <martomhidayat>
2022-11-10 14:57:27 +0700

    2 cara reverse EIGEN

--- src/test/Soal1.jsx
+++ src/test/Soal1.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
+//cara 1 replace
 const string = "NEGIE1"
 const replace = string.replace("NEGIE", "EIGEN");
+//cara 2 reverse
+const array = string.split('').reverse().join('').slice(1);
+const reverse = array+"1";
+
 
 const Soal1 = () => {
   return (
     <div>
         <h1>{replace}</h1>
+        <h1>{reverse}</h1>
     </div>
   )
 }
