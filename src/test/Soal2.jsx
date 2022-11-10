4d5bb5ffcd050911378703b7ea144d76e5edad71
micodee <martomhidayat>
2022-11-10 15:11:09 +0700

    mendapatkan kata terpanjang

--- src/test/Soal2.jsx
+++ src/test/Soal2.jsx
@@ -5,17 +5,21 @@ import React from 'react'
 // mengerjakan: 11 character
 
 const sentence = "Saya sangat senang mengerjakan soal algoritma"
+
+const longestWord = (sentence) => {
 //menyortir kata terpanjang
     const strArray = sentence.split(' ');
     const sortedStrArray = strArray.sort(
         (strA, strB) => {
             return strB.length - strA.length;
         }
-    )
+    );
+    return sortedStrArray[0]
+}
 
 const Soal2 = () => {
   return (
-    <div>{sortedStrArray}</div>
+    <div>{longestWord(sentence)}</div>
   )
 }
 
