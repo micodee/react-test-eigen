6b08ac750d9346eddeef0c813ee89c2b8755f0a3
micodee <martomhidayat>
2022-11-12 18:05:25 +0700

    Soal3 match data

--- src/test/Soal3.js
+++ src/test/Soal3.js
@@ -1,8 +1,29 @@
+import { getValue } from '@testing-library/user-event/dist/utils';
 import React from 'react'
+
+//Terdapat dua buah array yaitu array INPUT dan array QUERY, silahkan tentukan 
+//berapa kali kata dalam QUERY terdapat pada array INPUT
+const input = ['xc', 'dz', 'bbb', 'dz'];
+const query = ['bbb', 'ac', 'dz'];
+const matched = [];
+
+for (let index = 0; index < query.length; index++) {
+   for (let outerIndex = 0; outerIndex < input.length; outerIndex++) {
+      if (query[index].includes(input[outerIndex])) {
+         matched.push(input[outerIndex]);
+      }
+   }
+}
+
+
+
+
 
 const Soal3 = () => {
   return (
-    <div>Soal3</div>
+    <div>
+        {matched}
+    </div>
   )
 }
 
