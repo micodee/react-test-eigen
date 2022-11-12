da5048e292d825a8b65e480d770b0dbeef017db0
micodee <martomhidayat>
2022-11-12 08:15:25 +0700

    mengerjakan Soal3.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Soal1 from './test/Soal1'
 import Soal2 from './test/Soal2'
+import Soal3 from './test/Soal3'
 
 const App = () => {
   return (
     <>
         <Soal1/>
         <Soal2/>
+        <Soal3/>
     </>
   )
 }
---
+++ src/test/Soal3.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Soal3 = () => {
+  return (
+    <div>Soal3</div>
+  )
+}
+
+export default Soal3
