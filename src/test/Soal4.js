da657aa30ae6590850b8c8b12512543b0a4c2265
micodee <martomhidayat>
2022-11-30 19:04:05 +0700

    soal 4 done

--- src/test/Soal4.js
+++ src/test/Soal4.js
@@ -7,13 +7,25 @@ import React from "react";
 // diagonal kedua = 0 + 5 + 7 = 12
 
 const matrix = [[1, 2, 0], [4, 5, 6], [7, 8, 9]]
+const arr1 = matrix[0]
+const arr2 = matrix[1]
+const arr3 = matrix[2]
+const diagonal1 = arr1[0]+arr2[1]+arr3[2]
+const ar1 = matrix[0]
+const ar2 = matrix[1]
+const ar3 = matrix[2]
+const diagonal2 = [ar1[2]+ar2[1]+ar3[0]].toString()
+
+const output = diagonal1-diagonal2
 
 // maka hasilnya adalah 15 - 12 = 3
 
 const Soal4 = () => {
+  console.log(diagonal1)
   return (
     <div>
-      Soal 4
+      {diagonal1}-
+      {diagonal2}={output}
     </div>
   );
 };
